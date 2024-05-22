ebda7633c6ca337174eaae17261dbff627273064
shushinde111@gmail.com <shushinde111@gmail.com>
2024-05-22 12:31:16 +0530

    main updated

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 // import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {/* <Auth0Provider
-      domain="dev-at57wyi3mkj02gw5.us.auth0.com"
-      clientId="dHENpGHR8qM4AUrYH2tbOjvMYXZAYIrS"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    > */}
-    <App />
-    {/* </Auth0Provider> */}
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 reportWebVitals();
